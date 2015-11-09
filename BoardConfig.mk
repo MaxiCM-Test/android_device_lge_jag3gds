8cb964d87f4161dd5235c16e79d881e884bcb8c4
Andres Mora Montero <andresmora1676@gmail.com>
2015-11-09 07:33:31 -0600

    Add new kernel name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcmdhd.bin"
 # Kernel image
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/jag3gds/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/lge/jagnm
+TARGET_KERNEL_SOURCE := kernel/lge/msm8226
 TARGET_KERNEL_CONFIG := maxi_jag3gds_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=jag3gds user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
